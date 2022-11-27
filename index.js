ddd335a2b807dd0ace80fc4e93575debdf63b036
aybika <adamovaaybika@mail.ru>
2022-11-27 22:21:20 +0300

    back

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const app= express();
+const port = 3000;
+
+app.use(express.json());
+// app.use(express.urlencoded());
+app.use(require('./routes'));
+
+
+mongoose.connect("mongodb+srv://aybi:123@cluster0.lvirmgm.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log('Успешно соединились с сервером MongoDB'))
+  .catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
